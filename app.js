2a5ad8bcc80b446069f12de3b83ff379a515e0cf
DalenW <dalen.ward@gmail.com>
2017-10-05 23:08:05 -0600

    Header stuff, lets build this site.

--- app.js
+++ app.js
@@ -16,16 +16,5 @@ app.get("/", function(req, res) {
 });
 
 
-//certs stuff
-app.get("/.well-known/acme-challenge/jY0MLFQ5sUuXE1cd17G1mrina5w6b1OkjuKNQVARXqA", function(req, res) {
-    res.writeHead(200, {'content-type':'text/html'});
-    res.write("jY0MLFQ5sUuXE1cd17G1mrina5w6b1OkjuKNQVARXqA.MAsW9dYlCZ_d8NnS_Hc7GgvR-Tcd09iBz0huGctTRCE");
-});
-
-app.get("/.well-known/acme-challenge/Lf0HwI42XwQun4emQbhRctCnkmdIZyPYGkiq6cMqjJU", function(req, res) {
-    res.writeHead(200, {'content-type':'text/html'});
-    res.write("Lf0HwI42XwQun4emQbhRctCnkmdIZyPYGkiq6cMqjJU.MAsW9dYlCZ_d8NnS_Hc7GgvR-Tcd09iBz0huGctTRCE");
-});
-
 app.listen(8080);
 console.log("Started app");
