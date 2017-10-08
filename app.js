a7b38e8e44862f7fc1772726c5a8cf0e90dd8718
DalenW <dalen@Dalens-Hackintosh.local>
2017-10-07 23:19:08 -0600

    Added logs

--- app.js
+++ app.js
@@ -16,18 +16,22 @@ app.set("views", "./views");
 
 //------------  views  -----------
 app.get("/", function(req, res) {
+    console.log(`Got request for / from ${req.ip}`);
     res.render("index");
 });
 
 app.get("/about", function(req, res) {
+    console.log(`Got request for /about from ${req.ip}`);
     res.render("about");
 });
 
 app.get("/contact", function(req, res) {
+    console.log(`Got request for /contact from ${req.ip}`);
     res.render("contact");
 });
 
 app.get("/projects", function(req, res) {
+    console.log(`Got request for /projects from ${req.ip}`);
     res.render("projects");
 });
 
