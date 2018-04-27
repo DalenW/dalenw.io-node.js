296bb31dc9259a90a0bb2c4d2b78d49cc8182778
Dalen Ward <dalen.ward@gmail.com>
2018-04-27 09:49:25 -0400

    Rewriting some stuff

---
+++ app OLD.js
@@ -0,0 +1,46 @@
+//initialize libraries
+var express = require("express");
+var https = require("https");
+
+//set the variables
+var app = express();
+
+var options = {
+    cert: fs.readFileSync("./sslcert/fullchain.pem"),
+    key: fs.readFileSync("./sslcert/privkey.pem")
+};
+
+var port = 80
+
+//set the app
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+
+
+//------------  views  -----------
+app.get("/", function(req, res) {
+    console.log(`Got request for / from ${req.ip}`);
+    res.render("index");
+});
+
+app.get("/about", function(req, res) {
+    console.log(`Got request for /about from ${req.ip}`);
+    res.render("about");
+});
+
+app.get("/contact", function(req, res) {
+    console.log(`Got request for /contact from ${req.ip}`);
+    res.render("contact");
+});
+
+app.get("/projects", function(req, res) {
+    console.log(`Got request for /projects from ${req.ip}`);
+    res.render("projects");
+});
+
+
+app.listen(port);
+https.createServer(options, app).listen(8443);
+console.log("Started app on port: " + port);
--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-//initialize libraries
-var express = require("express");
-var https = require("https");
+var express = require("express")
 
-//set the variables
 var app = express();
 
-var options = {
-    cert: fs.readFileSync("./sslcert/fullchain.pem"),
-    key: fs.readFileSync("./sslcert/privley.pem")
-};
+var port = 8000;
 
-var port = 80
-
-//set the app
 app.set("view engine", "pug");
 app.set("views", "./views");
 app.use(express.static("public"));
-
-
 
 //------------  views  -----------
 app.get("/", function(req, res) {
@@ -25,22 +14,6 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.get("/about", function(req, res) {
-    console.log(`Got request for /about from ${req.ip}`);
-    res.render("about");
-});
-
-app.get("/contact", function(req, res) {
-    console.log(`Got request for /contact from ${req.ip}`);
-    res.render("contact");
-});
-
-app.get("/projects", function(req, res) {
-    console.log(`Got request for /projects from ${req.ip}`);
-    res.render("projects");
-});
-
 
 app.listen(port);
-https.createServer(options, app).listen(8443);
-console.log("Started app on port: " + port);
+console.log("Started on port: " + port);
