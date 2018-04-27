ecfc4e103af182bb608bbb704d5dccb83b0f467d
Dalen Ward <dalen.ward@gmail.com>
2018-04-27 18:17:18 -0400

    removed ssl crap

--- app.js
+++ app.js
@@ -29,12 +29,6 @@ app.get("/projects", function(req, res) {
     res.render("projects");
 });
 
-app.get("/.well-known/acme-challenge/czUaJv-0LvTtDhvlxZfrKdAFidTu2kF6GZZrSg9y0mQ", function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("czUaJv-0LvTtDhvlxZfrKdAFidTu2kF6GZZrSg9y0mQ.bQj8Gb_9NVh40Gs2qzD7Pbg1_sIWEBtZBEZu_b-d9Lg");
-    console.log("certbot A");
-});
-
 
 app.listen(port);
 console.log("Started on port: " + port);
